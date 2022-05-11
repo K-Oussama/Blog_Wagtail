2074b374a5be5e430e236c20f824a1ecf463b7b7
K-Oussama <oussama_korchi@um5.ac.ma>
2022-05-11 02:40:03 -0600

    init commit

---
+++ app/myapp/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-r6acdjzr&%d2d1#pkr(@j_qcd)o0g*%3rf^@x@3lzo7projzpt'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
